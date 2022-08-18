55bf3f863fb9191e710fb6b581dcdf4fec2b6cac
RedWirePlatinumTwo <85127483+RedWirePlatinumTwo@users.noreply.github.com>
2022-08-18 00:35:14 -0700

    Update FunnyUndertaleChatbox.lua

--- FunnyUndertaleChatbox.lua
+++ FunnyUndertaleChatbox.lua
@@ -15,7 +15,7 @@ Frame.BackgroundColor3 = Color3.new(0, 0, 0)
 Frame.BorderColor3 = Color3.new(1, 1, 1)
 Frame.BorderSizePixel = 3
 Frame.Position = UDim2.new(0.0911589488, 0, 0.0532612614, 0)
-Frame.Size = UDim2.new(0, 1142, 0, 160)
+Frame.Size = UDim2.new(0.800000012, 0, 0.150000006, 0)
 
 PlayerIcon.Name = "PlayerIcon"
 PlayerIcon.Parent = Frame
@@ -39,7 +39,7 @@ Message.TextWrapped = true
 Message.TextXAlignment = Enum.TextXAlignment.Left
 Message.TextYAlignment = Enum.TextYAlignment.Top
 -- Scripts:
-function SCRIPT_TJAD75_FAKESCRIPT() -- UTChatBox.Chatscript 
+function SCRIPT_OTWB86_FAKESCRIPT() -- UTChatBox.Chatscript 
 	local script = Instance.new('LocalScript')
 	script.Parent = UTChatBox
 	local gui = script.Parent
@@ -55,14 +55,11 @@ function SCRIPT_TJAD75_FAKESCRIPT() -- UTChatBox.Chatscript
 	local content = game.Players:GetUserThumbnailAsync(userId, thumbType, thumbSize)
 	Chatgui.Frame.PlayerIcon.Image = content
 	Chatgui.Enabled = true
-	local sentenceEnders = {".", "!", "?"}
 	local String2 = String:sub(1,1):upper()..String:sub(2)
-	if not table.find(sentenceEnders, String:sub(-1)) then
+	if not String:find("%.") and not String:find("?") and not String:find("!") then
 	    String2 = String2.."."
 	end
 	for i = 1, String2:len() do
-	local f = String2:sub(i,i)
-	if String2:len() ~= " " then
 	coroutine.resume(coroutine.create(function()
 	local s = Instance.new("Sound", workspace)
 	s.SoundId = "rbxassetid://5416666166"
@@ -71,15 +68,8 @@ function SCRIPT_TJAD75_FAKESCRIPT() -- UTChatBox.Chatscript
 	wait(s.TimeLength)
 	s:Destroy()
 	end))
-	end
 	Message.Text = "* "..String2:sub(1,i)
-	if f == "," and not tonumber(String2:sub(i-1,i-1)) then
-	wait(0.25)
-	elseif table.find(sentenceEnders,f) and not table.find(sentenceEnders,String2:sub(i+1,i+1)) then
-	wait(0.5)
-	else
 	wait()
-	end
 	end
 	wait(2)
 	Chatgui:Destroy()
@@ -89,4 +79,4 @@ function SCRIPT_TJAD75_FAKESCRIPT() -- UTChatBox.Chatscript
 	game.Players.PlayerAdded:connect(PlayerChatted)
 
 end
-coroutine.resume(coroutine.create(SCRIPT_TJAD75_FAKESCRIPT))
+coroutine.resume(coroutine.create(SCRIPT_OTWB86_FAKESCRIPT))
