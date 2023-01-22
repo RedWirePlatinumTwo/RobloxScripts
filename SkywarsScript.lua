72982205c0e0a8fcac5c047ac6911a43baec3f2d
RedWirePlatinumTwo <85127483+RedWirePlatinumTwo@users.noreply.github.com>
2023-01-22 02:21:13 -0800

    Update SkywarsScript.lua

--- SkywarsScript.lua
+++ SkywarsScript.lua
@@ -17,8 +17,8 @@ SkyWarsGUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 MainFrame.Name = "MainFrame"
 MainFrame.Parent = SkyWarsGUI
-MainFrame.BackgroundColor3 = Color3.new(0.333333, 0.666667, 1)
-MainFrame.BorderColor3 = Color3.new(0, 0, 1)
+MainFrame.BackgroundColor3 = Color3.new(0, 0.333333, 0)
+MainFrame.BorderColor3 = Color3.new(0, 0.666667, 0)
 MainFrame.Position = UDim2.new(0.385937482, 0, 0.0731481463, 0)
 MainFrame.Size = UDim2.new(0, 318, 0, 220)
 
@@ -32,101 +32,88 @@ Title.Text = "RedWire's Skywars GUI"
 Title.TextColor3 = Color3.new(0, 0.666667, 0)
 Title.TextScaled = true
 Title.TextSize = 14
-Title.TextStrokeColor3 = Color3.new(0, 0, 1)
 Title.TextStrokeTransparency = 0
 Title.TextWrapped = true
 
 mineores.Name = "mineores"
 mineores.Parent = MainFrame
-mineores.BackgroundColor3 = Color3.new(0.333333, 0.333333, 1)
-mineores.BorderColor3 = Color3.new(0, 0, 1)
+mineores.BackgroundColor3 = Color3.new(0, 0.333333, 0)
+mineores.BorderColor3 = Color3.new(0, 0.666667, 0)
 mineores.Position = UDim2.new(0, 0, 0.217523053, 0)
 mineores.Size = UDim2.new(0, 159, 0, 50)
 mineores.Font = Enum.Font.ArialBold
 mineores.Text = "Mine all ores"
 mineores.TextColor3 = Color3.new(0, 0.666667, 0)
-mineores.TextScaled = true
-mineores.TextSize = 14
-mineores.TextStrokeColor3 = Color3.new(0, 0, 1)
+mineores.TextSize = 20
 mineores.TextStrokeTransparency = 0
 mineores.TextWrapped = true
 
 highlightores.Name = "highlightores"
 highlightores.Parent = MainFrame
-highlightores.BackgroundColor3 = Color3.new(0.333333, 0.333333, 1)
-highlightores.BorderColor3 = Color3.new(0, 0, 1)
+highlightores.BackgroundColor3 = Color3.new(0, 0.333333, 0)
+highlightores.BorderColor3 = Color3.new(0, 0.666667, 0)
 highlightores.Position = UDim2.new(0.5, 0, 0.217523053, 0)
 highlightores.Size = UDim2.new(0, 159, 0, 50)
 highlightores.Font = Enum.Font.ArialBold
 highlightores.Text = "Highlight ores"
 highlightores.TextColor3 = Color3.new(0, 0.666667, 0)
-highlightores.TextScaled = true
-highlightores.TextSize = 14
-highlightores.TextStrokeColor3 = Color3.new(0, 0, 1)
+highlightores.TextSize = 20
 highlightores.TextStrokeTransparency = 0
 highlightores.TextWrapped = true
 
 automine.Name = "automine"
 automine.Parent = MainFrame
-automine.BackgroundColor3 = Color3.new(0.333333, 0.333333, 1)
-automine.BorderColor3 = Color3.new(0, 0, 1)
+automine.BackgroundColor3 = Color3.new(0, 0.333333, 0)
+automine.BorderColor3 = Color3.new(0, 0.666667, 0)
 automine.Position = UDim2.new(0, 0, 0.444137037, 0)
 automine.Size = UDim2.new(0, 159, 0, 50)
 automine.Font = Enum.Font.ArialBold
 automine.Text = "Auto-mining enabled: "
 automine.TextColor3 = Color3.new(0, 0.666667, 0)
-automine.TextScaled = true
-automine.TextSize = 14
-automine.TextStrokeColor3 = Color3.new(0, 0, 1)
+automine.TextSize = 20
 automine.TextStrokeTransparency = 0
 automine.TextWrapped = true
 
 megavipenter.Name = "megavipenter"
 megavipenter.Parent = MainFrame
-megavipenter.BackgroundColor3 = Color3.new(0.333333, 0.333333, 1)
-megavipenter.BorderColor3 = Color3.new(0, 0, 1)
+megavipenter.BackgroundColor3 = Color3.new(0, 0.333333, 0)
+megavipenter.BorderColor3 = Color3.new(0, 0.666667, 0)
 megavipenter.Position = UDim2.new(0.5, 0, 0.444137037, 0)
 megavipenter.Size = UDim2.new(0, 159, 0, 50)
 megavipenter.Font = Enum.Font.ArialBold
 megavipenter.Text = "Mega VIP bypass: "
 megavipenter.TextColor3 = Color3.new(0, 0.666667, 0)
-megavipenter.TextScaled = true
-megavipenter.TextSize = 14
-megavipenter.TextStrokeColor3 = Color3.new(0, 0, 1)
+megavipenter.TextSize = 20
 megavipenter.TextStrokeTransparency = 0
 megavipenter.TextWrapped = true
 
 infyeet.Name = "infyeet"
 infyeet.Parent = MainFrame
-infyeet.BackgroundColor3 = Color3.new(0.333333, 0.333333, 1)
-infyeet.BorderColor3 = Color3.new(0, 0, 1)
+infyeet.BackgroundColor3 = Color3.new(0, 0.333333, 0)
+infyeet.BorderColor3 = Color3.new(0, 0.666667, 0)
 infyeet.Position = UDim2.new(0, 0, 0.670750976, 0)
 infyeet.Size = UDim2.new(0, 159, 0, 50)
 infyeet.Font = Enum.Font.ArialBold
-infyeet.Text = "Load Infinite Yeet"
+infyeet.Text = "Load Infinite Yield"
 infyeet.TextColor3 = Color3.new(0, 0.666667, 0)
-infyeet.TextScaled = true
-infyeet.TextSize = 14
-infyeet.TextStrokeColor3 = Color3.new(0, 0, 1)
+infyeet.TextSize = 20
 infyeet.TextStrokeTransparency = 0
 infyeet.TextWrapped = true
 
 antifalltoggle.Name = "antifalltoggle"
 antifalltoggle.Parent = MainFrame
-antifalltoggle.BackgroundColor3 = Color3.new(0.333333, 0.333333, 1)
-antifalltoggle.BorderColor3 = Color3.new(0, 0, 1)
+antifalltoggle.BackgroundColor3 = Color3.new(0, 0.333333, 0)
+antifalltoggle.BorderColor3 = Color3.new(0, 0.666667, 0)
 antifalltoggle.Position = UDim2.new(0.5, 0, 0.670750976, 0)
 antifalltoggle.Size = UDim2.new(0, 159, 0, 50)
 antifalltoggle.Font = Enum.Font.ArialBold
-antifalltoggle.Text = "Anti-falling part enabled: "
+antifalltoggle.Text = "Anti-falling part enabled:"
 antifalltoggle.TextColor3 = Color3.new(0, 0.666667, 0)
-antifalltoggle.TextScaled = true
-antifalltoggle.TextSize = 14
-antifalltoggle.TextStrokeColor3 = Color3.new(0, 0, 1)
+antifalltoggle.TextSize = 20
 antifalltoggle.TextStrokeTransparency = 0
 antifalltoggle.TextWrapped = true
 -- Scripts:
-function SCRIPT_MBRZ87_FAKESCRIPT() -- SkyWarsGUI.LocalScript 
+function SCRIPT_RCBC86_FAKESCRIPT() -- SkyWarsGUI.LocalScript 
 	local script = Instance.new('LocalScript')
 	script.Parent = SkyWarsGUI
 	local lplr = game.Players.LocalPlayer
@@ -312,4 +299,4 @@ function SCRIPT_MBRZ87_FAKESCRIPT() -- SkyWarsGUI.LocalScript
 	end
 
 end
-coroutine.resume(coroutine.create(SCRIPT_MBRZ87_FAKESCRIPT))
+coroutine.resume(coroutine.create(SCRIPT_RCBC86_FAKESCRIPT))
