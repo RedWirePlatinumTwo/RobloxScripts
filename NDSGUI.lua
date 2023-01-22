0c320987db89a53acfd9b3df035f83cf223ffd95
RedWirePlatinumTwo <85127483+RedWirePlatinumTwo@users.noreply.github.com>
2023-01-22 02:15:29 -0800

    Update NDSGUI.lua

--- NDSGUI.lua
+++ NDSGUI.lua
@@ -17,91 +17,85 @@ RedWiresNDSGUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 MainFrame.Name = "MainFrame"
 MainFrame.Parent = RedWiresNDSGUI
 MainFrame.Active = true
-MainFrame.BackgroundColor3 = Color3.new(0.333333, 0.666667, 1)
-MainFrame.BorderSizePixel = 0
+MainFrame.BackgroundColor3 = Color3.new(0, 0.333333, 0.498039)
+MainFrame.BorderColor3 = Color3.new(0, 1, 1)
 MainFrame.Position = UDim2.new(0.63411206, 0, 0.140000343, 0)
-MainFrame.Size = UDim2.new(0, 322, 0, 199)
+MainFrame.Size = UDim2.new(0, 322, 0, 229)
 
 Falldamageremove.Name = "Falldamageremove"
 Falldamageremove.Parent = MainFrame
-Falldamageremove.BackgroundColor3 = Color3.new(1, 1, 1)
-Falldamageremove.Position = UDim2.new(0.200533465, 0, 0.695178032, 0)
-Falldamageremove.Size = UDim2.new(0, 197, 0, 45)
-Falldamageremove.Style = Enum.ButtonStyle.RobloxRoundDefaultButton
+Falldamageremove.BackgroundColor3 = Color3.new(0, 0.333333, 0.498039)
+Falldamageremove.BorderColor3 = Color3.new(0, 1, 1)
+Falldamageremove.Position = UDim2.new(0.188111112, 0, 0.791247904, 0)
+Falldamageremove.Size = UDim2.new(0, 197, 0, 40)
 Falldamageremove.Font = Enum.Font.SourceSansBold
 Falldamageremove.Text = "Remove Fall Damage"
-Falldamageremove.TextColor3 = Color3.new(1, 1, 1)
-Falldamageremove.TextScaled = true
+Falldamageremove.TextColor3 = Color3.new(0, 0.666667, 1)
 Falldamageremove.TextSize = 20
 Falldamageremove.TextWrapped = true
 
 prewarn.Name = "prewarn"
 prewarn.Parent = MainFrame
-prewarn.BackgroundColor3 = Color3.new(1, 1, 1)
-prewarn.Position = UDim2.new(-0.00132988405, 0, 0.258445859, 0)
-prewarn.Size = UDim2.new(0, 165, 0, 41)
-prewarn.Style = Enum.ButtonStyle.RobloxRoundDefaultButton
+prewarn.BackgroundColor3 = Color3.new(0, 0.333333, 0.498039)
+prewarn.BorderColor3 = Color3.new(0, 1, 1)
+prewarn.Position = UDim2.new(0.0141980648, 0, 0.258445829, 0)
+prewarn.Size = UDim2.new(0, 157, 0, 41)
 prewarn.Font = Enum.Font.SourceSansBold
 prewarn.Text = "Pre-Warn Disaster"
-prewarn.TextColor3 = Color3.new(1, 1, 1)
-prewarn.TextScaled = true
-prewarn.TextSize = 14
+prewarn.TextColor3 = Color3.new(0, 0.666667, 1)
+prewarn.TextSize = 20
 prewarn.TextWrapped = true
 
 Title.Name = "Title"
 Title.Parent = MainFrame
-Title.BackgroundColor3 = Color3.new(0.333333, 0.666667, 1)
+Title.BackgroundColor3 = Color3.new(0, 0.333333, 0.498039)
 Title.BorderSizePixel = 0
 Title.Size = UDim2.new(0, 320, 0, 49)
 Title.Font = Enum.Font.SourceSansBold
 Title.Text = "RedWire's Natural Disaster Survival GUI"
-Title.TextColor3 = Color3.new(1, 1, 1)
+Title.TextColor3 = Color3.new(0.333333, 0.666667, 1)
 Title.TextScaled = true
 Title.TextSize = 16
 Title.TextStrokeColor3 = Color3.new(0, 0.333333, 1)
-Title.TextStrokeTransparency = 0
 Title.TextWrapped = true
 
 ChatDisaster.Name = "ChatDisaster"
 ChatDisaster.Parent = MainFrame
-ChatDisaster.BackgroundColor3 = Color3.new(1, 1, 1)
-ChatDisaster.Position = UDim2.new(0.510982931, 0, 0.258445859, 0)
-ChatDisaster.Size = UDim2.new(0, 157, 0, 42)
-ChatDisaster.Style = Enum.ButtonStyle.RobloxRoundDefaultButton
+ChatDisaster.BackgroundColor3 = Color3.new(0, 0.333333, 0.498039)
+ChatDisaster.BorderColor3 = Color3.new(0, 1, 1)
+ChatDisaster.Position = UDim2.new(0.501775742, 0, 0.258445859, 0)
+ChatDisaster.Size = UDim2.new(0, 157, 0, 41)
 ChatDisaster.Font = Enum.Font.SourceSansBold
 ChatDisaster.Text = "Post Disaster In chat"
-ChatDisaster.TextColor3 = Color3.new(1, 1, 1)
-ChatDisaster.TextScaled = true
-ChatDisaster.TextSize = 14
+ChatDisaster.TextColor3 = Color3.new(0, 0.666667, 1)
+ChatDisaster.TextSize = 20
 ChatDisaster.TextWrapped = true
 
 Admin.Name = "Admin"
 Admin.Parent = MainFrame
-Admin.BackgroundColor3 = Color3.new(1, 1, 1)
-Admin.Position = UDim2.new(-0.00132988405, 0, 0.489147902, 0)
-Admin.Size = UDim2.new(0, 165, 0, 41)
-Admin.Style = Enum.ButtonStyle.RobloxRoundDefaultButton
+Admin.BackgroundColor3 = Color3.new(0, 0.333333, 0.498039)
+Admin.BorderColor3 = Color3.new(0, 1, 1)
+Admin.Position = UDim2.new(0.0141980667, 0, 0.515348732, 0)
+Admin.Size = UDim2.new(0, 157, 0, 41)
 Admin.Font = Enum.Font.SourceSansBold
 Admin.Text = "Infinite Yield"
-Admin.TextColor3 = Color3.new(1, 1, 1)
-Admin.TextScaled = true
-Admin.TextSize = 14
+Admin.TextColor3 = Color3.new(0, 0.666667, 1)
+Admin.TextSize = 20
 Admin.TextWrapped = true
 
 lobbytp.Name = "lobbytp"
 lobbytp.Parent = MainFrame
-lobbytp.BackgroundColor3 = Color3.new(1, 1, 1)
-lobbytp.Position = UDim2.new(0.510982931, 0, 0.489147902, 0)
-lobbytp.Size = UDim2.new(0, 156, 0, 41)
-lobbytp.Style = Enum.ButtonStyle.RobloxRoundDefaultButton
+lobbytp.BackgroundColor3 = Color3.new(0, 0.333333, 0.498039)
+lobbytp.BorderColor3 = Color3.new(0, 1, 1)
+lobbytp.Position = UDim2.new(0.501666129, 0, 0.515348732, 0)
+lobbytp.Size = UDim2.new(0, 157, 0, 41)
 lobbytp.Font = Enum.Font.SourceSansBold
 lobbytp.Text = "TP to lobby"
-lobbytp.TextColor3 = Color3.new(1, 1, 1)
-lobbytp.TextScaled = true
-lobbytp.TextSize = 14
+lobbytp.TextColor3 = Color3.new(0, 0.666667, 1)
+lobbytp.TextSize = 20
 lobbytp.TextWrapped = true
 -- Scripts:
-function SCRIPT_LEJU74_FAKESCRIPT() -- RedWiresNDSGUI.LocalScript 
+function SCRIPT_YBCO67_FAKESCRIPT() -- RedWiresNDSGUI.LocalScript 
 	local script = Instance.new('LocalScript')
 	script.Parent = RedWiresNDSGUI
 	local mframe = script.Parent.MainFrame
@@ -173,4 +167,4 @@ function SCRIPT_LEJU74_FAKESCRIPT() -- RedWiresNDSGUI.LocalScript
 	end)
 
 end
-coroutine.resume(coroutine.create(SCRIPT_LEJU74_FAKESCRIPT))
+coroutine.resume(coroutine.create(SCRIPT_YBCO67_FAKESCRIPT))
