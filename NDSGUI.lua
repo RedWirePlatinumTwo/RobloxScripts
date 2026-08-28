f642577abd07352dbbf8f42fdf1dffdba99cbbf8
RedWirePlatinumTwo <85127483+RedWirePlatinumTwo@users.noreply.github.com>
2026-08-28 02:03:03 -0700

    Adjust fall damage velocity limit to 65

--- NDSGUI.lua
+++ NDSGUI.lua
@@ -110,7 +110,7 @@ local function OQQJ_fake_script() -- RedWiresNDSGUI.LocalScript
 	local tcservice = game:GetService("TextChatService")
 	local txtchannel
 	local limitVelocity = false
-	local limiter = 69 -- no joke this is actually the limit before you take fall damage
+	local limiter = 65
 	for i,v in pairs(tcservice.TextChannels:GetChildren()) do
 		if v.Name:lower():find("general") then
 			txtchannel = v
